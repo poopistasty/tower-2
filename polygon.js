e0c21845c222f0460663d3368fa44dd96704ffe9
poopistasty <77624008+poopistasty@users.noreply.github.com>
2021-04-08 20:00:31 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,23 @@
+class Polygon{
+    constructor(x, y, width, height) {
+        var options = {
+        restitution:0.3,
+        friction:0.2             
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image=loadImage("polygon.png")
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width, this.height);
+        pop();
+      }
+}
